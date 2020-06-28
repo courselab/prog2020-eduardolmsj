279cb3c554b7e9e05a373f9657adcf3afe3a7d71
eduardolmsj <dudump10111@usp.br>
2020-06-28 23:30:28 +0000

    exercicio 5

--- m005.c
+++ m005.c
@@ -1,4 +1,4 @@
-/* m005.c - Determinant.
+ /* m005.c - Determinant.
 
    This program reads the integer coefficients of a 3x3 matrix
    and outputs the matrix's determinant. Matrix coefficients
@@ -26,8 +26,10 @@
 /* Return the determinant of a 3x3 integer matrix.*/
 
 int determinant (int m[3][3])
-{
-  return 0;
+{ int n, x;
+  n = (m[0][0]*m[1][1]*m[2][2])+(m[0][1]*m[1][2]*m[2][0])+(m[0][2]*m[1][0]*m[2][1]);
+  x = (m[2][0]*m[1][1]*m[0][2])+(m[2][1]*m[1][2]*m[0][0])+(m[2][2]*m[1][0]*m[0][1]);
+  return n-x;
 }
 
 
