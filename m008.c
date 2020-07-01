76060c8dfa0a4948cd368d03460014c52130724e
eduardolmsj <dudump10111@usp.br>
2020-07-01 17:48:57 +0000

    aleluia exercicio 8

--- m008.c
+++ m008.c
@@ -28,8 +28,100 @@
 /* Converts the string 's' from first-middle-last name 
    form into LAST-first-middle form. */
 
+int space_counter(char *s)
+{
+  int i, n = 0;
+  
+  for(i = 0; s[i] != 0; i++)
+  {
+    if (s[i] == ' ')
+     n++;
+  }
+  return n;
+}
+
 void lastname (char *s)
 {
+  char fname[100], mnames[500], lname[100], lnameinv[100];
+  int i, n, j, k, z, w, x, ae, teste;
+
+  k = 0;
+  n = space_counter(s);
+
+  for (i = 0; s[i] != ' '; i++) //primeiro nome
+   {
+     fname[i] = s[i];
+   }
+  fname[i] = 0;
+  //printf ("%s %d %d\n", fname, i, n);
+
+  i++;
+  
+  for (j = 0; s[j] != 0; j++)  //o tamanho da string s
+     {}
+  j-=2;
+
+  while(s[j] != ' ' && s[j] != 0)  //ultimo nome ao contrario
+  {
+    //printf("%d\n", k);
+    lnameinv[k] = s[j];
+    k++;
+    j--;
+  } 
+
+  lnameinv[k] = 0;
+  //printf("%s %d\n", lnameinv, k);
+  w = k;
+  k--;
+
+  for(z = 0; z < w; z++) //ultimo nome
+  {
+    lname[z] = lnameinv[k];
+    k--;
+  }
+  lname[z] = 0;
+  //printf("%s\n", lname);
+
+  x = 0;
+
+  while (i < j) //nomes do meio
+   {
+     mnames[x] = s[i];
+     x++;
+     i++;
+   }
+   mnames[x] = 0;
+   //printf("%s\n", mnames);
+
+   teste = 0;
+
+   for(ae = 0; lname[ae] != 0; ae++)
+   {
+     if(lname[ae]>='A' && lname[ae]<='Z')
+     s[ae] = lname[ae];
+     if(lname[ae]>='a' && lname[ae]<='z')
+     s[ae] = lname[ae] - ('a'-'A');
+   }
+   s[ae] = ',';
+   ae+= 2;
+   while(fname[teste] != 0)
+   {
+     s[ae] = fname[teste];
+     teste++;
+     ae++;
+   }
+   s[ae] = ' ';
+   teste = 0;
+   ae++;
+   while(mnames[teste] != 0)
+   {
+     s[ae] = mnames[teste];
+     teste++;
+     ae++;
+   }
+   s[ae] = 0;
+
+   
 }
 
 
