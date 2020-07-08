99eae3ac02f6c166ad4ac1096cdbfd79011082c3
eduardolmsj <64203046+eduardolmsj@users.noreply.github.com>
2020-07-07 21:40:07 -0300

    exercicio 10

--- m010.c
+++ m010.c
@@ -30,7 +30,63 @@ enum {sun, mon, tue, wed, thu, fri, sat};
 
 int day_of_week (int day, int month)
 {
-  return sun;
+   //primeiro dia foi quarta
+  int i, j;
+
+
+  switch(month)
+  {
+    case 1:
+      month = 0;
+      break;
+    case 2:
+      month = 31;
+      break;
+    case 3:
+      month = 60;
+      break;
+    case 4: 
+      month = 91;
+      break;
+    case 5:
+      month = 121;
+      break;
+    case 6:
+      month = 152;
+      break;
+    case 7:
+      month = 182;
+      break;
+    case 8:
+      month = 213;
+      break;
+    case 9:
+      month = 244;
+      break;
+    case 10:
+      month = 274;
+      break;
+    case 11:
+      month = 305;
+      break;
+    case 12:
+      month = 335;
+      break;
+  }
+
+  if(day>=1 && day<=7)
+    i = 2 + month + day;
+  if(day>=8 && day<=14)
+     i = 2 + month + day-7;
+  if(day>=15 && day<=21)
+     i = 2 + month + day-14;
+  if(day>=22 && day<=28)
+    i = 2 + month + day-21; 
+  if(day>=29 && day<=31)
+     i = 2 + month + day-28;
+  
+  j = i%7;
+  return j;
 }
 
 /* Do not edit function main. */
