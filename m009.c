53019077e85fde18d79e8652892a00c4289fd2fe
eduardolmsj <64203046+eduardolmsj@users.noreply.github.com>
2020-07-07 21:38:22 -0300

    exercicio 9

--- m009.c
+++ m009.c
@@ -25,86 +25,23 @@
 
 void sort (int* vector, int n)
 {
-   char fname[100], mnames[500], lname[100], lnameinv[100];
-  int i, j, k, z, w, x, ae, teste;
+   int i, j, menor, x;
 
-  k = 0;
-
-  for (i = 0; s[i] != ' '; i++) //primeiro nome
-   {
-     fname[i] = s[i];
-   }
-  fname[i] = 0;
-  //printf ("%s %d %d\n", fname, i, n);
-
-  i++;
-  
-  for (j = 0; s[j] != 0; j++)  //o tamanho da string s
-     {}
-  j-=2;
-
-  while(s[j] != ' ' && s[j] != 0)  //ultimo nome ao contrario
+  for(i = 0; i<n; i++)
   {
-    //printf("%d\n", k);
-    lnameinv[k] = s[j];
-    k++;
-    j--;
-  } 
-
-  lnameinv[k] = 0;
-  //printf("%s %d\n", lnameinv, k);
-  w = k;
-  k--;
-
-  for(z = 0; z < w; z++) //ultimo nome
-  {
-    lname[z] = lnameinv[k];
-    k--;
+    menor = i;
+    for(j = i+1; j < n; j++)
+    {
+      if(vector[j]<vector[menor])
+        menor = j;
+    }
+    if(vector[menor]!=vector[i])
+    {
+       x = vector[i];
+       vector[i] = vector[menor];
+       vector[menor] = x;
+    }
   }
-  lname[z] = 0;
-  //printf("%s\n", lname);
-
-  x = 0;
-
-  while (i < j) //nomes do meio
-   {
-     mnames[x] = s[i];
-     x++;
-     i++;
-   }
-   mnames[x] = 0;
-   //printf("%s\n", mnames);
-
-   teste = 0;
-
-   for(ae = 0; lname[ae] != 0; ae++) //troca-troca
-   {
-     if(lname[ae]>='A' && lname[ae]<='Z')
-     s[ae] = lname[ae];
-     if(lname[ae]>='a' && lname[ae]<='z')
-     s[ae] = lname[ae] - ('a'-'A');
-   }
-   s[ae] = ',';
-   s[ae+1] = ' ';
-   ae+= 2;
-   while(fname[teste] != 0)
-   {
-     s[ae] = fname[teste];
-     teste++;
-     ae++;
-   }
-   s[ae] = ' ';
-   teste = 0;
-   ae++;
-   while(mnames[teste] != 0)
-   {
-     s[ae] = mnames[teste];
-     teste++;
-     ae++;
-   }
-   s[ae] = 0;
-
-   
 }
 
 #define USAGE "m009 <num1> <nun2> ... \n"
